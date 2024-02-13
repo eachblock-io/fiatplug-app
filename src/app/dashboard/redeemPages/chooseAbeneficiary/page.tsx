da78e0a19fa5563922c013d50bb9b312901edbb7
unknown <unclebignay@hashnode.com>
2024-02-13 23:09:39 +0100

    feat: redeem-page

--- src/app/dashboard/redeemPages/chooseAbeneficiary/page.tsx
+++ src/app/dashboard/redeemPages/chooseAbeneficiary/page.tsx
@@ -7,9 +7,7 @@ import { IoIosArrowForward } from "react-icons/io";
 import { useState } from "react";
 import { RiBankLine } from "react-icons/ri";
 
-
 const chooseAbeneficiary = () => {
-
   return (
     <section className=" lg:w-11/12 w-11/12 mx-auto mt-6">
       <div className="lg:w-8/12 w-11/12 mx-auto">
@@ -18,40 +16,36 @@ const chooseAbeneficiary = () => {
           href="/"
           className="bg-white border shadow-lg lg:p-8 p-2 flex gap-x-4 items-center justify-between rounded-[2rem] my-8 ">
           <div className="flex items-center lg:gap-x-6 gap-x-4">
-            <p className="lg:text-lg text-sm">
-              Start typing to filter.....
-            </p>
+            <p className="lg:text-lg text-sm">Start typing to filter.....</p>
           </div>
         </Link>
 
-            <Link href="/dashboard/redeemPages/redeemPoints/" >
-                    <div className="bg-white border shadow-lg lg:p-8 p-6 rounded-[2rem] mb-5">
-                            <div className="p-2  rounded-lg flex">
+        <Link href="/dashboard/redeemPages/redeemPoints/">
+          <div className="bg-white border shadow-lg lg:p-8 p-6 rounded-[2rem] mb-5">
+            <div className="p-2  rounded-lg flex">
+              <div className="inline-block  rounded-full py-5 px-5 bg-[#28B825]">
+                <RiBankLine className=" text-white" />
+              </div>
 
-                                <div className="inline-block  rounded-full py-5 px-5 bg-[#28B825]">
-                                    <RiBankLine className=" text-white"/>
-                                </div>
+              <div>
+                <p>Jams, Ethan Hunt</p>
+                <span>2023434318 | Opay</span>
+              </div>
+            </div>
+          </div>
+        </Link>
 
-                                <div>
-                                    <p>Jams, Ethan Hunt</p>
-                                    <span>2023434318 | Opay</span>
-                                </div>
-                            </div>
-                    </div>
-                </Link>
-        
         <div className="bg-white border shadow-lg lg:p-8 p-6 rounded-[2rem]">
-                <div className="p-2  rounded-lg flex">
+          <div className="p-2  rounded-lg flex">
+            <div className="inline-block rounded-full py-5 px-5 bg-[#28B825]">
+              <RiBankLine className=" text-white" />
+            </div>
 
-                     <div className="inline-block rounded-full py-5 px-5 bg-[#28B825]">
-                            <RiBankLine className=" text-white"/>
-                        </div>
-
-                    <div>
-                        <p>Jams, Ethan Hunt</p>
-                        <span>2023434318 | Opay</span>
-                      </div>
-                 </div>
+            <div>
+              <p>Jams, Ethan Hunt</p>
+              <span>2023434318 | Opay</span>
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- src/app/dashboard/redeemPages/redeemPoints/page.tsx
+++ src/app/dashboard/redeemPages/redeemPoints/page.tsx
@@ -3,76 +3,73 @@ import React from "react";
 import { IoCloseCircleSharp } from "react-icons/io5";
 import { FaArrowRight } from "react-icons/fa6";
 import { Input } from "@/components/ui/input";
-import magicStar from '../../../public/magic_star_.png'
+import magicStar from "../../../public/magic_star_.png";
 import { Button } from "@/components/ui/button";
 import { RiBankLine } from "react-icons/ri";
 
 const redeemPoints = () => {
   return (
     <>
-
       <FaArrowRight className="text-white" />
       <div className="lg:w-5/12 w-12/12 mx-auto pb-10 lg:mt-10 mt-20">
         <h1 className="font-bold text-2xl">Redeem points for cash</h1>
         <p>Where should we send to?</p>
         <form className="space-y-6 mt-8">
-        <Link href="/" passHref>
-          <div className="p-2 border border-gray-300 rounded-lg flex">
-
-                  <div className="inline-block  rounded-[9px] py-5 px-5 mr-5 bg-[#FFA048]">
-                      <img src="/magic_star_.png" alt="" className=" w-8 h-8" />
-                    </div>
-                  <div>
-                    <p>Beneficiaries</p>
-                    <span>Send to already saved channels</span>
-                  </div>
-          </div>
+          <Link href="/" passHref>
+            <div className="p-2 border border-gray-300 rounded-lg flex">
+              <div className="inline-block  rounded-[9px] py-5 px-5 mr-5 bg-[#FFA048]">
+                <img src="/magic_star_.png" alt="" className=" w-8 h-8" />
+              </div>
+              <div>
+                <p>Beneficiaries</p>
+                <span>Send to already saved channels</span>
+              </div>
+            </div>
           </Link>
-                    <div className="bg-white border shadow-lg lg:p-8 p-6 rounded-[2rem] mb-5">
-                            <div className="p-2  rounded-lg flex">
+          <div className="bg-white border shadow-lg lg:p-8 p-6 rounded-[2rem] mb-5">
+            <div className="p-2  rounded-lg flex">
+              <div className="inline-block  rounded-full py-5 px-5 mr-5 bg-[#28B825]">
+                <RiBankLine className=" text-white" />
+              </div>
 
-                                <div className="inline-block  rounded-full py-5 px-5 mr-5 bg-[#28B825]">
-                                    <RiBankLine className=" text-white"/>
-                                </div>
-
-                                <div>
-                                    <p>Jams, Ethan Hunt</p>
-                                    <span>2023434318 | Opay</span>
-                                </div>
-                            </div>
-                    </div>
+              <div>
+                <p>Jams, Ethan Hunt</p>
+                <span>2023434318 | Opay</span>
+              </div>
+            </div>
+          </div>
           <div>
             <span>Enter points to redeem</span>
-            <div className="relative flex items-center"> 
-            <Input
-              type="text"
-              id="accountName"
-              name="accountName"
-              required
-              placeholder="Account Number"
-              className="pl-16 pr-16 py-2 border border-gray-300 w-full" 
-            />
-      <span className="absolute left-4 top-0 bottom-0 flex items-center pl-2">0</span>
-      <span className="absolute right-4 top-0 bottom-0 flex items-center pr-2">MAX</span>
-    </div>
+            <div className="relative flex items-center">
+              <Input
+                type="text"
+                id="accountName"
+                name="accountName"
+                required
+                placeholder="Account Number"
+                className="pl-16 pr-16 py-2 border border-gray-300 w-full"
+              />
+              <span className="absolute left-4 top-0 bottom-0 flex items-center pl-2">
+                0
+              </span>
+              <span className="absolute right-4 top-0 bottom-0 flex items-center pr-2">
+                MAX
+              </span>
+            </div>
             <div className="flex justify-between">
-              <div>
-              </div>
+              <div></div>
               <p>Balance 550</p>
             </div>
           </div>
-          <Button
-            className="relative bg-[#F9A21B] hover:bg-[#f3b456] px-4 lg:h-12 lg:text-md mt-4 rounded-full flex items-center w-full">
-            
+          <Button className="relative bg-[#F9A21B] hover:bg-[#f3b456] px-4 lg:h-12 lg:text-md mt-4 rounded-full flex items-center w-full">
             {/* Text container centered */}
             <span className="w-full text-center block">Redeem Points</span>
-            
+
             {/* Right icon aligned to the right */}
             <FaArrowRight className="text-white absolute right-4" />
           </Button>
         </form>
       </div>
-      
     </>
   );
 };
