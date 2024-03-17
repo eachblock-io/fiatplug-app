5e37cb5be07bb220c043e0b02edb2207bf4d46f8
unknown <unclebignay@hashnode.com>
2024-03-17 17:23:34 +0100

    feat: error

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+"use client";
+import { useEffect, useState } from "react";
 import { Sidenav } from "@/components/Sidenav";
 import Mobilenav from "@/components/Mobilenav";
 import Header from "@/components/Header";
+import axios, { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+
+async function getUser(): Promise<userResponse> {
+  try {
+    const { data } = await axios.get("/api/me");
+
+    return {
+      user: data?.user,
+      error: null,
+    };
+  } catch (e) {
+    const error = e as AxiosError;
+
+    return {
+      user: null,
+      error,
+    };
+  }
+}
+
+
+interface userResponse {
+  user: String | null;
+  error: AxiosError | null;
+}
+
 
 const Layout = ({
   children,
@@ -10,12 +38,29 @@ const Layout = ({
   children: React.ReactNode | React.ReactElement;
   data: any;
 }) => {
+
+  const { push } = useRouter();
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [user, setUser] = useState<any>({});
+
+  useEffect(() => {
+    (async (async) => {
+      const { user, error } = await getUser();
+      setUser(user);
+      if (error) {
+        push("/");
+        return;
+      }
+
+      setIsSuccess(true);
+    })();
+  }, [push]);
   return (
     <section className="flex sm:h-screen h-screen w-full overflow-hidden">
-      <Sidenav data={data?.attributes} />
+      <Sidenav data={user?.attributes} />
       <Mobilenav />
       <main className="w-full relative overflow-y-auto">
-        <Header data={data} />
+        <Header data={user} />
         {children}
       </main>
     </section>
